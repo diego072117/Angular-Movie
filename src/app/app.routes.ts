e05fe61448b19df5edb1385c0f420407d0c46c58
diego072117 <parracalderond9@gmail.com>
2025-01-22 18:59:49 -0500

    feat(search): search movies.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { DetailsMovieComponent } from './pages/details-movie/details-movie.component';
+import { SearchMovieComponent } from './pages/search-movie/search-movie.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'movie/:id', component: DetailsMovieComponent },
+    { path: 'search', component: SearchMovieComponent },
 ];
